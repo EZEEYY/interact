00ba4cf019ee0c379893643330cff3d2792c965f
EZEEYY <aayushshr357@gmail.com>
2023-01-12 18:38:16 +0545

    terve

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,14 @@
 import { useGlobalContext } from "../Context"
-import {BsHandThumbsUp,BsHandThumbsUpFill} from 'react-icons/bs'
+import {BsHandThumbsUp , BsHandThumbsUpFill} from 'react-icons/bs'
+import { useState } from "react"
 const Meals =()=>{
     const {meals} = useGlobalContext()
     console.log(meals)
+    const [btn,btnup] = useState(<BsHandThumbsUp/>)
+    function update(){
+        btnup(<BsHandThumbsUpFill/>)
+    }
+
     return(
         <div className="foods">
             {meals.map((meal)=>{
@@ -11,7 +17,7 @@ const Meals =()=>{
                     <img src={meal.strMealThumb} style={{width:'300px'}} className='img'></img>
                     <footer className="foot">
                         <p> {meal.strMeal}</p>
-                        <button className="btn">{<BsHandThumbsUp/>}</button>
+                        <button className="btn" onClick={update}>{btn}</button>
                     </footer>
                 </div>)
             })}
